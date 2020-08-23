d0999d2acd13b957df4f21a4ce514ab5c9384277
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2020-08-23 15:52:31 +0600

    Set COOCIE and SESSION

--- home.php
+++ home.php
@@ -2,31 +2,29 @@
 
 <!-- echo "<br />"; -->
 <?php
-$all_keys = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-'0','1','2','3','4','5','6','7','8','9','@','#','$','%','^','*','-','_','+','(',')','',
-',','.',':',';','?','/','!');
+$name = "username";
+$value = "karim";
+setcookie($name, $value, time()+20);
 
-$length=array(5,6,7,8,9,10,11,12,13,14,15);
+if(isset($_COOKIE['username'])){
+echo "Saved COOKIE is= {$_COOKIE['username']}";   
+}else{
+    echo "After 20 seconds COOKIE is gone!";
 
-shuffle($length);
 
-$final_length = $length[0];
 
-echo "<br />Password length is=$final_length<br /><br />";
-$str = "";
+  
 
-for($i=0; $i<$final_length; $i++){
+    echo "<br />";
 
-    shuffle($all_keys);
+    session_start();
+    $_SESSION['username'] = "karim";
+    echo "SESSION is running=";
+    echo  $_SESSION['username'];
 
-    $str .=$all_keys[0];  //$str=$str.$all_keys;
 
-    echo "<br />$str";
+
 }
-
-echo "<br />Your final random password is=$str";
-
 ?>
 
  <?php include('include/footer.php');?>
