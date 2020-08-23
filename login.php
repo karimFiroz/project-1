73e9be42820bbd86a39c45421d4ddf76202c856e
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2020-08-23 20:57:44 +0600

    Create Success

--- login.php
+++ login.php
@@ -1,17 +1,33 @@
 <?php include('include/header.php');?>
 <?php 
 
-if(isset($_REQUEST['wrongPass'])){
-    echo $_REQUEST['wrongPass'];
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email= $_POST['email'];
+    $password= $_POST['password'];
+   $connection = mysqli_connect('localhost','root','','users');
+   if(!$connection){
+die("Not Connected.". mysqli_error());
+   }
+
+   $query ="INSERT INTO user_info(username,email,password) 
+   VALUES('$username','$email','$password')";
+ $result = mysqli_query($connection,$query);
+  
+   if($result){
+       echo "Insert success.";
+   }else{
+       echo "Isnert Fail.";
+   }
 }
 
 ?>
 
-<form action="home.php" method="GET"><br />
+<form action="login.php" method="POST"><br />
 <input type="text" name="username" placeholder="username"><br />
 <input type="email" name="email" placeholder="email"><br />
 <input type="password" name="password" placeholder="password"><br />
-<input type="submit" value="submit">
+<input type="submit" name="submit" value="submit">
 </form>
 
 
