a82643b8a889eeb84ed534c9976c23c900747627
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2020-08-26 05:05:07 +0600

    insert page created to upload page from database

--- insert.php
+++ insert.php
@@ -3,7 +3,17 @@
 <!-----------    <?php echo "<br />" ?> -->
 
 <?php 
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $receive_file=$_FILES['upload_image'];
+    $image_name=$receive_file['name'];
+    $image_tmp_name['tmp_name'];
 
+
+
+}
 $connection = mysqli_connect('localhost','root','','users');
 if(!$connection){
     die("Not Connection.". mysqli_error($connection));
@@ -20,11 +30,21 @@ if(!$connection){
             echo "<font color='green'><h3>Data is updated!</h3></font>";
         }
       ?>
+<form action="insert.php" method="POST" enctype="multipart/form-data">
+<input type="text" name="username" placeholder="username">
+<input type="email" name="email" placeholder="email">
+<input type="password" name="password" placeholder="password">
+<input type="file" name="upload_image"  value="upload">
+<input type="submit" name="submit" value="submit">
+</form>
+<br />
+<br />
       <table class="table">
     <thead class="head-dark">
         <tr>
         <th>SN</th>
             <th>ID</th>
+            <th>Profile_pic</th>
             <th>Username</th>
             <th>E-mail</th>
             <th>Password</th>
@@ -45,6 +65,8 @@ if(!$connection){
         <tr>
         <td><?php echo $serial_No; ?></td>
             <td><?php echo "$db_id"; ?></td>
+            <td><img src="img/profile
+            .jpg"width:50px></td>
             <td><?php echo "$username"; ?></td>
             <td><?php echo "$email"; ?></td>
             <td><?php echo "$password"; ?></td>
